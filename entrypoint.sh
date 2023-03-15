c46d104b5f66f94f5cf60d2a374ad0b7e40fab9f
mp33 <mp33@sanger.ac.uk>
2023-03-15 13:20:30 +0000

    Set DISPLAY back to :1

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,18 +26,21 @@ if [[ -z "${NOVNC_PORT}" ]]; then
   NOVNC_PORT=`python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'`
 fi
 
+#export DISPLAY=":$RANDOM"
+export DISPLAY=:1
+
 # show access information
 echo -e "#--------------------------------------------------------------------------------"
 echo -e "#-- 🌐 Browse address:\thttp://$(hostname).internal.sanger.ac.uk:${NOVNC_PORT}"
 echo -e "#-- 🔐 noVNC password:\t${NOVNC_PASSWORD}" 
 echo -e "#--------------------------------------------------------------------------------"
-echo -e "#-- 📓 Before launch configuration if you want custom values use:"
+echo -e "#-- 📓 For custom values use these options before launch:"
 echo -e "#--  ├─ Port: 'export NOVNC_PORT=<PORT_NUMBER>'"
 echo -e "#--  └─ Password: 'export NOVNC_PASSWORD=<PASSWORD>'"
 echo -e "#--------------------------------------------------------------------------------"
 
 # create unix domain socket on which vncserver listens for connections from websockify
-VNC_SOCKET="$HOME/.vnc/.socket"
+#VNC_SOCKET="$HOME/.vnc/.socket"
 
 # launch websockifly start vncserver and exec virtualgl sessionxfc4 
 # TODO try unix sockets: --unix-target=${VNC_SOCKET} / -rfbunixpath ${VNC_SOCKET} 
@@ -51,4 +54,5 @@ VNC_SOCKET="$HOME/.vnc/.socket"
       -rfbport ${NOVNC_PORT} \
       -rfbauth ${VNC_PASSWD} \
       -securitytypes vnc -xstartup \
-      /opt/xstartup # $DISPLAY
+      /opt/xstartup $DISPLAY
+
